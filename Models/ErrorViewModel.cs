b6dec94f69ca82afac5740997353a33cda077bde
Gabriel Mier <gabrilmartins5@gmail.com>
2022-11-28 21:26:49 -0300

    atualização

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aula_exemplo_123.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
